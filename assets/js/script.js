6fd2eed3ab5128e332887313be3a709253569323
James Xalis <jamesxalis@icloud.com>
2022-09-25 16:35:43 -0700

    moving to different computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+const quiz = document.getElementById ('quiz')
+const answerEls = document.querySelectorAll ('.answer')
+const questionEls = document.querySelectorAll ('.question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+const startBtn = document.getElementById('starting-button')
+
+
 const quizData =[
     {
         question: "Which language runs in a web browser?",
@@ -33,37 +44,27 @@ const quizData =[
     },
 ]
 
-const quiz = document.getElementById ('quiz')
-const answerEls = document.querySelectorAll ('.answer')
-const questionEls = document.querySelectorAll ('.question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
 
 let currentQuiz = 0
 let score = 0
 
-loadQuiz()
+function deselectAnswers(){
+
+    answerEls.forEach(answerEls => answerEls.checked = false)
+}
 
 function loadQuiz(){
     
     deselectAnswers()
-
+    
     const currentQuizData = quizData[currentQuiz]
-
+    
     questionEls.innerText = currentQuizData.question
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
-
-}
-
-function deselectAnswers(){
-    answerEls.forEach(answerEls => answerEls.checked = false)
-
+    
 }
 
 function getSelected() {
@@ -72,20 +73,20 @@ function getSelected() {
         if(answerEls.checked){
             answer=answerEls.id
         }
-
+        
     })
     return answer
 }
 
-submitBtn.addEventListener (), () =>{
+startBtn.addEventListener ('click'), () =>{
     const answer = getSelected()
     if(answer){
         if (answer === quizData[currentQuiz].correct){
             score++
         }
-
+        
         currentQuiz ++
-
+        
         if(currentQuiz < quizData.length){
             loadQuiz()
         }else{
@@ -94,6 +95,5 @@ submitBtn.addEventListener (), () =>{
             <button on click="location.reload()">Reload</button>
             `
         }
-    }
-
+    }    
 }
