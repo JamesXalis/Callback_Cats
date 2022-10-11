39a815f43d844b643f5931f95e20fa9c7ebd6d86
James Xalis <jamesxalis@icloud.com>
2022-10-11 14:37:45 -0700

    Added questions and js page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var initialTime = 50;
+var time = 50;
+var score = 0;
+var quizCount = 0;
+var setTime;
+var answers = document.querySelectorAll('#quizHolder button');
+var clock;
+var recordsArray = [];
